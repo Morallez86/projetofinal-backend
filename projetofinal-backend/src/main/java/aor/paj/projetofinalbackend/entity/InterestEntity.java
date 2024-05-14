296bba2d19cfc42ffab2e92d6065f7124561dc59
Joao Morais <joaomorais@student.dei.uc.pt>
2024-05-14 15:03:54 +0100

    backup2 entities. Falta histórico e chat de grupo

--- projetofinal-backend/src/main/java/aor/paj/projetofinalbackend/entity/InterestEntity.java
+++ projetofinal-backend/src/main/java/aor/paj/projetofinalbackend/entity/InterestEntity.java
@@ -1,14 +1,12 @@
 package aor.paj.projetofinalbackend.entity;
 
 import jakarta.persistence.*;
-
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name="interest")
-
 public class InterestEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -27,38 +25,72 @@ public class InterestEntity implements Serializable {
     @ManyToMany(mappedBy = "interests")
     private Set<UserEntity> users = new HashSet<>();
 
+    @ManyToMany(mappedBy = "interests")
+    private Set<ProjectEntity> projects = new HashSet<>();
+
     public InterestEntity() {
+        // Default constructor
     }
+
+    // Getters and Setters
 
     public Long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public UserEntity getCreator() {
-        return creator;
-    }
-
-    public Set<UserEntity> getUsers() {
-        return users;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public UserEntity getCreator() {
+        return creator;
     }
 
     public void setCreator(UserEntity creator) {
         this.creator = creator;
     }
 
+    public Set<UserEntity> getUsers() {
+        return users;
+    }
+
     public void setUsers(Set<UserEntity> users) {
         this.users = users;
+    }
+
+    public Set<ProjectEntity> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<ProjectEntity> projects) {
+        this.projects = projects;
+    }
+
+    public void addProject(ProjectEntity project) {
+        this.projects.add(project);
+        project.getInterests().add(this);
+    }
+
+    public void removeProject(ProjectEntity project) {
+        this.projects.remove(project);
+        project.getInterests().remove(this);
+    }
+
+    public void addUser(UserEntity user) {
+        this.users.add(user);
+        user.getInterests().add(this);
+    }
+
+    public void removeUser(UserEntity user) {
+        this.users.remove(user);
+        user.getInterests().remove(this);
     }
 }
