ddf2e56332b46808cc4878a6c7a110e3a1361ff7
Joao Morais <joaomorais@student.dei.uc.pt>
2024-05-24 12:08:51 +0100

    Correcção do ficheiro base dados

--- projetofinal-backend/src/main/resources/imports.sql
+++ projetofinal-backend/src/main/resources/imports.sql
@@ -1,6 +1,6 @@
 INSERT INTO user (first_name, last_name, username, password, email, role, active, pending, visibility) VALUES
     ('John', 'Doe', 'johndoe', 'password123', 'johndoe@example.com', 'U', true, false, true);
 
-INSERT INTO skill (name, type, creator_id) VALUES ('Java', 200, @user_id);
+INSERT INTO skill (name, type, creator_id) VALUES ('Java', 'SOFTWARE', 1);
 
 INSERT INTO user_skill (user_id, skill_id) VALUES (1, 1);
