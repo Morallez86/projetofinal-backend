87d1e238b221bcc8863255097a41c16601a5c6b2
ricardoelias98 <uc2016225298@student.uc.pt>
2024-05-28 15:55:18 +0100

    mais skills add no user

--- projetofinal-backend/src/main/resources/imports.sql
+++ projetofinal-backend/src/main/resources/imports.sql
@@ -5,6 +5,10 @@ INSERT INTO skill (name, type, creator_id) VALUES ('Java', 'SOFTWARE', 1);
 INSERT INTO skill (name, type, creator_id) VALUES ('Coding', 'KNOWLEDGE', 1);
 INSERT INTO skill (name, type, creator_id) VALUES ('Placas Gráficas', 'HARDWARE', 1);
 INSERT INTO skill (name, type, creator_id) VALUES ('Parafusos', 'TOOLS', 1);
+INSERT INTO skill (name, type, creator_id) VALUES ('Git', 'KNOWLEDGE', 1);
+INSERT INTO skill (name, type, creator_id) VALUES ('React', 'KNOWLEDGE', 1);
+INSERT INTO skill (name, type, creator_id) VALUES ('Kraken', 'KNOWLEDGE', 1);
+INSERT INTO skill (name, type, creator_id) VALUES ('VSCode', 'KNOWLEDGE', 1);
 
 INSERT INTO interest (creator_id, name) VALUES (1, 'CyberSecurity');
 INSERT INTO interest (creator_id, name) VALUES (1, 'Animals');
@@ -12,3 +16,9 @@ INSERT INTO interest (creator_id, name) VALUES (1, 'Math');
 INSERT INTO interest (creator_id, name) VALUES (1, 'Nature');
 
 INSERT INTO user_skill (user_id, skill_id) VALUES (1, 1);
+INSERT INTO user_skill (user_id, skill_id) VALUES (1, 3);
+INSERT INTO user_skill (user_id, skill_id) VALUES (1, 9);
+INSERT INTO user_skill (user_id, skill_id) VALUES (1, 10);
+INSERT INTO user_skill (user_id, skill_id) VALUES (1, 11);
+INSERT INTO user_skill (user_id, skill_id) VALUES (1, 12);
+
