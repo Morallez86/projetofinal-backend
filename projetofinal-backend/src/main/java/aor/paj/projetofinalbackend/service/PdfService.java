b19c77f1ea7b5f1eefba804c4d337d4e69dfa506
ricardoelias98 <uc2016225298@student.uc.pt>
2024-07-06 14:01:25 +0100

    logs no PDF Service

--- projetofinal-backend/src/main/java/aor/paj/projetofinalbackend/service/PdfService.java
+++ projetofinal-backend/src/main/java/aor/paj/projetofinalbackend/service/PdfService.java
@@ -1,11 +1,16 @@
 package aor.paj.projetofinalbackend.service;
 
 import aor.paj.projetofinalbackend.bean.PdfBean;
+import aor.paj.projetofinalbackend.dao.TokenDao;
+import aor.paj.projetofinalbackend.entity.UserEntity;
+import aor.paj.projetofinalbackend.utils.LoggerUtil;
 import com.itextpdf.text.DocumentException;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.time.LocalDateTime;
 
 @Path("/pdf")
 public class PdfService {
@@ -13,12 +18,18 @@ public class PdfService {
     @Inject
     private PdfBean pdfBean;
 
+    @Inject
+    TokenDao tokenDao;
+
     @GET
     @Path("/generate")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public Response generatePdf(@HeaderParam("Authorization") String authorizationHeader) {
+        String token = authorizationHeader.substring("Bearer".length()).trim();
+        UserEntity user = tokenDao.findUserByTokenValue(token);
         try {
             byte[] pdfContents = pdfBean.generatePdf();
+            LoggerUtil.logInfo("GENERATE PDF", "at " + LocalDateTime.now(),user.getEmail(), token);
             return Response.ok(pdfContents)
                     .header("Content-Disposition", "attachment; filename=\"application_statistics.pdf\"")
                     .build();
